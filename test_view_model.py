14d0924b33ff14b50a3d4d2fe436e6ae9140d981
Toby <tobyrowe@me.com>
2021-04-07 15:14:57 +0100

    Add additional test

--- test_view_model.py
+++ test_view_model.py
@@ -1,12 +1,13 @@
 from todo_item import TodoItem
 from view_model import ViewModel
+import arrow
 
 def test_to_do():
-    # Arrange
+    # Passing through blank value for the Date Input as not required for this test
     items = [
-        TodoItem(1, "Started Todo", "Not Started"),
-        TodoItem(2, "Doing Todo", "In Progress"),
-        TodoItem(3, "Done Todo", "Completed"),
+        TodoItem(1, "Started Todo", "Not Started", ""),
+        TodoItem(2, "Doing Todo", "In Progress", ""),
+        TodoItem(3, "Done Todo", "Completed", ""),
     ]
 
     frank = ViewModel(items)
@@ -25,9 +26,9 @@ def test_to_do():
 def test_doing():
     # Arrange
     items = [
-        TodoItem(1, "Started Todo", "Not Started"),
-        TodoItem(2, "Doing Todo", "In Progress"),
-        TodoItem(3, "Done Todo", "Completed"),
+        TodoItem(1, "Started Todo", "Not Started", ""),
+        TodoItem(2, "Doing Todo", "In Progress", ""),
+        TodoItem(3, "Done Todo", "Completed", ""),
     ]
 
     vader = ViewModel(items)
@@ -46,9 +47,9 @@ def test_doing():
 def test_done():
 
     items = (
-        TodoItem(1, "Started Todo", "Not Started"), 
-        TodoItem(2, "Doing Todo", "In Progress"),
-        TodoItem(3, "Done Todo", "Completed"),
+        TodoItem(1, "Started Todo", "Not Started", ""), 
+        TodoItem(2, "Doing Todo", "In Progress", ""),
+        TodoItem(3, "Done Todo", "Completed", ""),
     )
     storm = ViewModel(items)
 
@@ -64,9 +65,9 @@ def test_done():
 def test_show_all_done_items_returns_true_for_small_numbers_of_items():
     # Arrange
     items = [
-        TodoItem(1, "Started Todo", "Not Started"),
-        TodoItem(2, "Doing Todo", "In Progress"),
-        TodoItem(3, "Done Todo", "Completed"),
+        TodoItem(1, "Started Todo", "Not Started", ""),
+        TodoItem(2, "Doing Todo", "In Progress", ""),
+        TodoItem(3, "Done Todo", "Completed", ""),
     ]
 
     vader = ViewModel(items)
@@ -80,14 +81,14 @@ def test_show_all_done_items_returns_true_for_small_numbers_of_items():
 def test_show_all_done_items_returns_false_for_large_numbers_of_items():
     # Arrange
     items = [
-        TodoItem(1, "Started Todo", "Not Started"),
-        TodoItem(2, "Doing Todo", "In Progress"),
-        TodoItem(3, "Done Todo", "Completed"),
-        TodoItem(4, "Done Todo", "Completed"),
-        TodoItem(5, "Done Todo", "Completed"),
-        TodoItem(6, "Done Todo", "Completed"),
-        TodoItem(7, "Done Todo", "Completed"),
-        TodoItem(8, "Done Todo", "Completed"),
+        TodoItem(1, "Started Todo", "Not Started", ""),
+        TodoItem(2, "Doing Todo", "In Progress", ""),
+        TodoItem(3, "Done Todo", "Completed", ""),
+        TodoItem(4, "Done Todo", "Completed", ""),
+        TodoItem(5, "Done Todo", "Completed", ""),
+        TodoItem(6, "Done Todo", "Completed", ""),
+        TodoItem(7, "Done Todo", "Completed", ""),
+        TodoItem(8, "Done Todo", "Completed", ""),
     ]
 
     vader = ViewModel(items)
@@ -97,3 +98,26 @@ def test_show_all_done_items_returns_false_for_large_numbers_of_items():
     
     # Assert
     assert result == False
+
+def test_show_all_done_items_today():
+
+    today = arrow.utcnow().floor('day').format('YYYY-MM-DD')
+    
+    items = (
+        TodoItem(1, "Started Todo", "Not Started", today), 
+        TodoItem(2, "Doing Todo", "In Progress", today),
+        TodoItem(3, "Done Todo", "Completed", today),
+        TodoItem(4, "Done Todo", "Completed", "2021-04-06")
+    )
+    egg = ViewModel(items)
+
+    # Act
+    result = egg.show_all_done_items_today
+    
+    # Assert
+    assert len(result) == 1
+
+    done_item = result[0]
+
+    assert done_item.status == "Completed"
+    assert done_item.id == 3
--- view_model.py
+++ view_model.py
@@ -55,14 +55,14 @@ class ViewModel:
     @property
     def show_all_done_items_today(self):
         # shows all items that have been clompleted today
-        print("Correct Function")
-        print(self._items)
+        #print("Correct Function")
+        #print(self._items)
         output = []
 
         for item in self._items:
-            print(item.date)
-            print(arrow.utcnow())
-            print(item.title)
+            #print(item.date)
+            #print(arrow.utcnow())
+            #print(item.title)
             if item.status == "Completed" and arrow.get(item.date).floor('day') == arrow.utcnow().floor('day'):
                 output.append(item)
 
